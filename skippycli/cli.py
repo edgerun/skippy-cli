969e45e67459cfa5f147314dbd81bc341331c77b
Thomas Rausch <thomas@rauschig.org>
2020-07-01 16:07:19 +0200

    Rename skippy command group

--- skippycli/cli.py
+++ skippycli/cli.py
@@ -5,19 +5,19 @@ import click
 
 @click.group()
 @click.option('--debug/--no-debug', default=False)
-def symmetry(debug):
+def skippy(debug):
     if debug:
         click.echo('Debug mode is %s' % ('on' if debug else 'off'))
         logging.basicConfig(level=logging.DEBUG)
 
 
-@symmetry.command()
+@skippy.command()
 def build():
     print('creating skippy.yaml')
 
 
 def main(*args, **kwargs):
-    symmetry(*args, **kwargs)
+    skippy(*args, **kwargs)
 
 
 if __name__ == '__main__':
