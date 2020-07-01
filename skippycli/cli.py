21ce09f86255b3adea6adff27ce28b3790d9a98c
Thomas Rausch <thomas@rauschig.org>
2020-07-01 16:06:19 +0200

    Use click for cli stub

--- skippycli/cli.py
+++ skippycli/cli.py
@@ -1,5 +1,24 @@
-def main():
-	print('hello world')
+import logging
+
+import click
+
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def symmetry(debug):
+    if debug:
+        click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+        logging.basicConfig(level=logging.DEBUG)
+
+
+@symmetry.command()
+def build():
+    print('creating skippy.yaml')
+
+
+def main(*args, **kwargs):
+    symmetry(*args, **kwargs)
+
 
 if __name__ == '__main__':
-	main()
+    main()
